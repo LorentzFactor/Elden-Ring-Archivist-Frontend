19cee4d27c67e149b5d1f11faeaab0d81ac3a2f8
Colman Bouton <cole.bouton@gmail.com>
2024-07-24 11:42:26 -0700

    Remove unused code from search.tsx

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { redirect, json } from '@remix-run/node';
-import { useLoaderData, Link, useSearchParams, Outlet } from '@remix-run/react';
+import { useSearchParams, Outlet } from '@remix-run/react';
 import SearchBar from '../components/SearchBar';
 
-/*export const loader = async ({ request }) => {
-  const url = new URL(request.url);
-  const searchTerm = url.searchParams.get('q') || '';
-  // If the search param has been specified, go to results page
-  if (searchTerm) {return redirect('/search/results?q=' + searchTerm)};
-  return null;
-};*/
-
 const Search = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
   return (
     <div>
-      <SearchBar/>
+      <SearchBar className="py-px-10 margin-10"/>
       <Outlet/>
     </div>
   );
