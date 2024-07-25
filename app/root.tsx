c3c5689298a312c32ac797068098a0864a725151
Colman Bouton <cole.bouton@gmail.com>
2024-07-24 18:36:52 -0700

    Improve header, footer

--- app/root.tsx
+++ app/root.tsx
@@ -35,16 +35,19 @@ export const meta: MetaFunction = () => {
         </head>
         <body className="overscroll-contain">
           <div className="bg-zinc-300 overscroll-contain">
-          <h1 className="sticky text-3xl font-bold top">The Silver Tear</h1>
-          <div className="static min-h-screen">
-            <Outlet />
-          </div>
-          <div className="relative bg-zinc-800 text-white w-screen bottom-0 h-12 p-0 m-0">
-            <div className="flex absolute size-12 inset-x-1/2">
-                <Link className="size-min pr-4" to="/search">Search</Link>
-                <Link className=" size-min pl-4" to="/about">About</Link>
+          <h1 className="sticky top-0 text-3xl font-bold bg-zinc-800 text-white p-4 shadow-md w-full z-50">
+            The Silver Tear
+          </h1>
+            <div className="static min-h-screen">
+              <Outlet />
             </div>
-          </div>
+            <div className="relative bg-zinc-800 text-white w-screen bottom-0 h-12 p-4 m-0 shadow-md  flex justify-center items-center">
+              <p className="flex gap-2 text-lg">
+                <Link className="hover:text-gray-300" to="/search">Search</Link>
+                <span>|</span>
+                <Link className="hover:text-gray-300" to="/about">About</Link>
+              </p>
+            </div>
           </div>
           <Scripts />
           <LiveReload />
