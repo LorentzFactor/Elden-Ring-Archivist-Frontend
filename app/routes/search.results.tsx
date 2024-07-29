2d845ded3967c4b489d3226a63d19cc27a72c447
Colman Bouton <cole.bouton@gmail.com>
2024-07-29 16:23:06 -0700

    Add loading spinner to search

--- app/routes/search.results.tsx
+++ app/routes/search.results.tsx
@@ -1,9 +1,11 @@
 import { React, Suspense} from 'react';
-import { useLoaderData } from '@remix-run/react';
+import { Await, useLoaderData } from '@remix-run/react';
 import default_index from '../utils/pineconeClient';
 import openai from '../utils/openAIClient';
-import { redirect } from '@remix-run/node';
+import { redirect, defer } from '@remix-run/node';
 import SearchResultsContainer from '../components/SearchResultsTable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 type SearchResult = {
   id: string,
@@ -12,50 +14,59 @@ type SearchResult = {
 
 const preamble_string="Answer the following question about the lore of the game Elden Ring, using information provided from a data dump of the game's item text.\nPay particular attention to the Caption field, if there is one, as this often contains the most lore.\n\nQuestion:\n"
 
-export const loader = async ({ request }) => {
+export const loader = async ({ params }) => {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get('q');
+  console.log(searchTerm);
   if (!searchTerm) {
     throw redirect('/search')
   }
   
-  const embedding_response = await openai.embeddings.create({
+  const embedding_response_promise = openai.embeddings.create({
     model: "text-embedding-3-large",
     input: preamble_string + searchTerm,
     encoding_format: "float",
   });
 
-  const query_vec = embedding_response.data[0].embedding;
+  const query_vec_promise = embedding_response_promise.then((embedding_response) => {return embedding_response.data[0].embedding});
   
-  const data_matches = await default_index.query({
+  const data_matches_promise = query_vec_promise.then((query_vec)=> {return default_index.query({
     vector: query_vec, 
     topK: 10,
     filter: { $and:[{"Name": {$exists: true}}, {"Caption": {$exists: true}}] },
     includeMetadata: true
-  })
+  })});
 
-  const data: any[] = []
-  for (let match of data_matches.matches) {
-    let item_data = match.metadata!
-    item_data.id = match.id
-    data.push(item_data)
-  }
+  const data_promise = data_matches_promise.then((data_matches) => {
+    let data: any[] = [];
+    for (let match of data_matches.matches) {
+      let item_data = match.metadata!;
+      item_data.id = match.id;
+      data.push(item_data);
+    };
+    return data;
+  });
 
-  return { data };
+  return defer({'data': data_promise});
 };
+
+function LoadingResults() {
+  return (
+    <FontAwesomeIcon icon={faSpinner} className="text-center text-zinc-500 h-20 w-20 mx-auto animate-spin"/>
+  )
+}
 
 const SearchResult = () => {
     const { data } = useLoaderData<typeof loader>();
-    if (!data || data.length === 0) {
-        return <div>No data available</div>;
-      }
-    
-      // Get table headers
-      const headers = ["Name", "Caption", "Info"]
+    console.log("loading new data")
     
       return (
         <div className="container mx-auto">
-          <SearchResultsContainer data={data}/>
+          <Suspense fallback={<LoadingResults />}>
+            <Await resolve={data}>
+              {(data)=> <SearchResultsContainer data={data}/>}
+            </Await>
+          </Suspense>
         </div>
       ); 
 };
