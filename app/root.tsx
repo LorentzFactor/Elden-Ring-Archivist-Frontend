33378b0189b5a7cdc2bd117ef90c7bb2ced41536
Colman Bouton <cole.bouton@gmail.com>
2024-07-30 12:10:10 -0700

    Add about link to header, include some additional formatting

--- app/root.tsx
+++ app/root.tsx
@@ -34,17 +34,20 @@ export const meta: MetaFunction = () => {
           <Links />
         </head>
         <body className="overscroll-contain">
-          <header className="sticky top-0 bg-zinc-800 p-4 shadow-md w-full z-50 flex flex-row items-center justify-left space-x-4">
+          <header className="sticky top-0 bg-zinc-800 p-4 shadow-md w-full z-50 flex flex-row items-center space-x-4">
             <img src={bannerimage} alt="The Silver Tear Banner" className="max-h-20" />
             <Link prefetch="render" to="/search" className="text-4xl font-bold bg-transparent text-white hover:text-zinc-200 transition-transform hover:scale-105">The Silver Tear</Link>
+            <div className="grow place-self-end pb-0 text-right">
+              <Link prefetch="hover" to="/about" className=" text-2xl bg-transparent text-violet-400 hover:text-zinc-200 ">About</Link>
+            </div>
           </header>
           <div className="static min-h-screen">
             <Outlet />
           </div>
-          <div className="relative bg-zinc-800 text-white w-screen bottom-0 h-12 p-4 m-0 shadow-md flex justify-center items-center">
+          <div className="relative bg-zinc-800 text-violet-400 w-screen bottom-0 h-12 p-4 m-0 shadow-md flex justify-center items-center">
             <p className="flex gap-2 text-lg">
               <Link className="hover:text-gray-300" to="/search">Search</Link>
-              <span>|</span>
+              <span className="text-zinc-100">|</span>
               <Link className="hover:text-gray-300" to="/about">About</Link>
             </p>
           </div>
