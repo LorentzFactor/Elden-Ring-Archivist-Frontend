77978da4c15491defb8d6bf58692b68939159067
Colman Bouton <cole.bouton@gmail.com>
2024-07-30 11:08:30 -0700

    Switch from gray to zinc text to match website theme

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div className="max-w-4xl mx-auto p-5">
-            <h1 className="text-4xl font-bold text-gray-800 mb-4">About</h1>
-            <p className="text-lg text-gray-600">
+            <h1 className="text-4xl font-bold text-zinc-800 mb-4">About</h1>
+            <p className="text-lg text-zinc-600">
                 This is a personal project by <a href="https://github.com/LorentzFactor" className="text-blue-500 hover:text-blue-700 transition duration-300">@LorentzFactor</a> (aka Colman Bouton).
             </p>
         </div>
