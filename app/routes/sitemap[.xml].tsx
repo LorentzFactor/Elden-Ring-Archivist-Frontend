43d4f412c7e7ad35a926e52535c3aa49f5505617
Colman Bouton <cole.bouton@gmail.com>
2024-08-19 13:53:37 -0700

    Auto update sitemap to include previous search history.

--- app/routes/sitemap[.xml].tsx
+++ app/routes/sitemap[.xml].tsx
@@ -1,7 +1,25 @@
 import { json } from "@remix-run/node";
 import { LoaderFunction } from "@remix-run/node";
+import createRedisClient from '../utils/redisClient';
+
+const searchToXMLSiteMap = async () => {
+  const redisClient = await createRedisClient();
+  // Fetch all ips and convert them to the key used to store searches
+  const ips = await redisClient.sMembers("known_ips").then(ips => ips.map(ip => `searches:by_ip:${ip}`));
+  // Fetch all searches for each ip
+  const searches = await redisClient.sUnion(ips);
+  // Close the connection to the redis server
+  await redisClient.disconnect();
+  // url encode each search and conver them to urls
+  const urls = searches.map(search => `https://the-silver-tear.com/search/${encodeURIComponent(search)}`);
+  // Lastly, convert the urls to an xml block
+  const urlsAsXml= urls.map(url => `<url><loc>${url}</loc><priority>0.5</priority></url>`).join('\n');
+
+  return urlsAsXml;
+}
 
 export let loader: LoaderFunction = async () => {
+  const urlsAsXml = await searchToXMLSiteMap();
   const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>https://the-silver-tear.com/search/</loc>
@@ -13,6 +31,7 @@ export let loader: LoaderFunction = async () => {
         <lastmod>2024-08-01</lastmod>
         <priority>0.8</priority>
       </url>
+      ${urlsAsXml}
     </urlset>`;
 
   return new Response(sitemap, {
