2ad4b85d2435742788f10d341b3d55b393d342b7
Colman Bouton <cole.bouton@gmail.com>
2024-09-11 15:45:34 -0700

    Remove results table edge padding for small screens

--- app/components/SearchResultsTable.tsx
+++ app/components/SearchResultsTable.tsx
@@ -59,7 +59,7 @@ const SearchResultsContainer = ({ data }: SearchResultsContainerProps) => {
     }
 
     return (
-        <ul className="container drop-shadow-xl mx-auto grid grid-cols-1 gap-y-1.5 w-3/4 bg-zinc-300 p-3 rounded-lg">
+        <ul className="container drop-shadow-xl mx-auto grid grid-cols-1 gap-y-1.5 md:w-3/4 bg-zinc-300 p-3 rounded-lg">
             {data.map((row: ItemData) => (
                 <li key={row.ID}>
                     <SearchResult item_data={row} />
